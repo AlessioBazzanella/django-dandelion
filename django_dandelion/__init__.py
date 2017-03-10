4332b11321b9c1645ab1f2bca08056e5626fdc41
Alessio Bazzanella <alessio.bazzanella@me.com>
2017-03-10 04:03:28 +0100

    version 0.1.1

--- django_dandelion/__init__.py
+++ django_dandelion/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 default_app_config = 'django_dandelion.apps.DandelionConfig'
